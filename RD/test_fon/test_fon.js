fa57816bbc4ed16c6fa74c7adb1aae1d0f606ae2
Ваня <43142140+na-163c@users.noreply.github.com>
2018-09-10 20:56:33 +0300

    Improved visual perception of the code

---
+++ RD/test_fon/test_fon.js
@@ -0,0 +1,24 @@
+var muz = new Audio();
+muz.currentTime = 0;
+muz.src = "Lilium.mp3";
+
+var bool = true;
+
+
+
+function oneuse() {
+    if (bool){
+        window.open("https://vk.com/id479121650");
+    } else {
+            }
+            bool = false;
+}
+
+
+function play() {
+    oneuse()
+    muz.paused ? muz.play() : muz.pause()
+
+}
+
+
