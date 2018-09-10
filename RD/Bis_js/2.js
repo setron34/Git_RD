abf12f61875983f9d316da643c5180e7bf0e38ea
Ваня <43142140+na-163c@users.noreply.github.com>
2018-09-10 17:38:40 +0300

    Add files via upload

---
+++ RD/Bis_js/2.js
@@ -0,0 +1,1 @@
+$(document).ready(function(){$(".sidebox").has(".searchForm").addClass("srch");$(".show-menu").bind("click",function(){$(this).siblings(".vert-menu").slideToggle("slow");$(this).toggleClass("opened")});$(".vert-menu li:has(ul) > a").addClass("sub-mn");$("a.logo-b").click(function(a){$(a.target).hasClass("uz-signs")&&a.preventDefault()})});
